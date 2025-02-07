e5454e249286c5c59e1336202762c6e25953eb0f
Ebrahim Al Aghbari <alaghbaryebraheem1@gmail.com>
2025-02-07 22:13:14 +0000

    rename clean_submission

---
+++ src/clean_submission.py
@@ -0,0 +1,15 @@
+class CleanSubmission:
+    def __init__():
+        pass
+    
+    def unzip(self):
+        pass
+
+    def rename(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def move(self):
+        pass
