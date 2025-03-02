815ab6663144d9d19b806477cd25e0c2a3798eaf
Ebrahim Al Aghbari <alaghbaryebraheem1@gmail.com>
2025-03-02 22:46:02 +0000

    simple modifications

--- src/main.py
+++ src/main.py
@@ -1,25 +1,23 @@
 from venv_manager import VenvManager
-from test_manager import overrideFiles, run_task
+from test_manager import SubmissionPreprocessor, run_task
+from pathlib import Path
 
 
-def grade_single_submission(student_dir: str):
+def grade_single_submission(student_dir: Path):
     # Paths
     # teacher_dir = "teacher"
-    # student_path = os.path.join("submissions", student_dir)
-    
+    # student_path = os.path.join("submissions", student_dir)    
     # 1. Initialize venv
     venv = VenvManager(student_dir)
     venv.create_venv()
-    
-
     venv.install_requirements()
     
     # 3. Test Task_1 (example)
     try:
         # Override files and run test
-        print(run_task("Task_1", r'/Users/ebrahim_alaghbari/Documents/Portifolio/Automated-Marking/tests/TemplatePythonModel', student_dir, venv))
+        print(run_task("Task_2", student_dir, venv))
     except Exception as e:
         print(f"Error grading Task 1: {str(e)}")
 
 if __name__ == "__main__":
-    grade_single_submission(r'/Users/ebrahim_alaghbari/Library/CloudStorage/OneDrive-CardiffUniversity/CM3203 - Individual Project/Portfolio 2 Marking Support/TemplatePythonModel')  # Grade a specific submission
+    grade_single_submission(Path("tests/Cleaned_Test_Files/Portfolio 2 Upload Zone_c000000"))  # Grade a specific submission
