39b16e36dc7c60ff8721c3ba920fc65b37fb2b57
Ebrahim Al Aghbari <Alaghbarie@cardiff.ac.uk>
2025-03-07 05:31:48 +0000

    choir

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from venv_manager import VenvManager
 from test_manager import SubmissionPreprocessor, run_task
 from pathlib import Path
+from report_generate import generate_detailed_report
 
-
-def grade_single_submission(student_dir: Path):
+def grade_single_submission(task, student_dir: Path):
     # Paths
     # teacher_dir = "teacher"
     # student_path = os.path.join("submissions", student_dir)    
-    # 1. Initialize venv
+    # # 1. Initialize venv
+    # SubmissionPreprocessor(r'TemplatePythonModel', student_dir).process_single_submission(student_dir)
     venv = VenvManager(student_dir)
     venv.create_venv()
     venv.install_requirements()
@@ -15,9 +16,21 @@ def grade_single_submission(student_dir: Path):
     # 3. Test Task_1 (example)
     try:
         # Override files and run test
-        print(run_task("Task_2", student_dir, venv))
+        print(run_task(task, student_dir, venv))
+
+        
     except Exception as e:
-        print(f"Error grading Task 1: {str(e)}")
+        print(f"Error grading {task}: {str(e)}")
+    generate_detailed_report(1, r"tests\Cleaned_Test_Files\Portfolio 2 Upload Zone_c000000")
+
+def grade_all_submissions(task, submissions_dir = r"..\tests\Cleaned_Test_Files"):
+    sub_dir = Path(submissions_dir)
+    
+    for submission in sub_dir.iterdir():
+        grade_single_submission(task, submission)
+
+        generate_detailed_report(1, submission)
+
 
 if __name__ == "__main__":
-    grade_single_submission(Path("tests/Cleaned_Test_Files/Portfolio 2 Upload Zone_c000000"))  # Grade a specific submission
+    grade_single_submission("Task_1",Path(r"tests\Cleaned_Test_Files\Portfolio 2 Upload Zone_c000000"))  # Grade a specific submission
